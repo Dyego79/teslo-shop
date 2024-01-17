7f0b043d9a2e712dd09cf4b5630ecec6323504b8
Dyego79 <95590936+Dyego79@users.noreply.github.com>
2024-01-17 16:17:59 -0300

    prisma folder

---
+++ prisma/migrations/20231227032935_product/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `size` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `slu` on the `Product` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[slug]` on the table `Product` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `slug` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "Product_slu_key";
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "size",
+DROP COLUMN "slu",
+ADD COLUMN     "sizes" "Size"[] DEFAULT ARRAY[]::"Size"[],
+ADD COLUMN     "slug" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
